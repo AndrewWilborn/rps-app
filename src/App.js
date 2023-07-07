cb1e3d91c28126f56fe005bd39ea5654a95bd235
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-07 15:29:49 -0400

    ADDED choose layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useState } from 'react';
 import './styles/App.css';
 import Choose from './pages/Choose';
 
 function App() {
+
+  const [selectedItem, setSelectedItem] = useState()
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Choose /> } />
+        <Route path='/' element={ <Choose setSelectedItem={setSelectedItem} selectedItem={selectedItem}/> } />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Choose.jsx
+++ src/pages/Choose.jsx
@@ -1,10 +1,17 @@
 import AppLayout from "../Layout/AppLayout";
 
-export default function Choose() {
+
+export default function Choose({ setSelectedItem, selectedItem}) {
 
     return(
         <AppLayout>
             <h1>Player vs Computer</h1>
+            <div>
+                <img src="/images/rockR.webp" alt="" onClick={() => setSelectedItem(0)}/>
+                <img src="/images/scissorsR.png" alt="" onClick={() => setSelectedItem(1)}/>
+                <img src="/images/paper.png" alt="" onClick={() =>setSelectedItem(2)}/>
+            </div>
+            <button></button>
         </AppLayout>
     )
 }
