bed67685c29dcd35fbb30ff5747ed60b4cae8217
Andrew Wilborn <andrewwilborn@Andrews-MacBook-Air-2.local>
2023-07-07 17:45:25 -0400

    UPDATED functionality

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -3,6 +3,36 @@ import { useNavigate } from "react-router-dom";
 
 const imgMap = ["/images/rockL.webp", "/images/scissorsL.png", "/images/paper.png"]
 
+function getWinMessage(selectedItem, cpuItem){
+    if(selectedItem === cpuItem){
+        return "Its a Tie!"
+    }
+    if(selectedItem === 0){
+        if(cpuItem === 2){
+            return "Paper beats Rock, you lose."
+        }
+        else{
+            return "Rock beats Scissors, you win!"
+        }
+    }
+    if(selectedItem === 1){
+        if(cpuItem === 0){
+            return "Rock beats Scissors, you lose."
+        }
+        else{
+            return "Scissors beats Paper, you win!"
+        }
+    }
+    if(selectedItem === 2){
+        if(cpuItem === 1){
+            return "Scissors beats Paper, you lose."
+        }
+        else{
+            return "Paper beats Rock, you win!"
+        }
+    }
+}
+
 export default function Results({selectedItem}){
     const Nav = useNavigate()
 
@@ -12,7 +42,7 @@ export default function Results({selectedItem}){
 
     useEffect(() => {
         const x = setInterval(() => {
-            const res = window.confirm("continue");
+            const res = window.confirm(getWinMessage(selectedItem, cpuChoice));
             if(res){
                 Nav("/")
             }
@@ -25,7 +55,7 @@ export default function Results({selectedItem}){
     useEffect(() => {
         const x = setInterval(() => {
             setImg1(imgMap[selectedItem])
-            
+            setImg2(imgMap[cpuChoice])
         }, 1000);
         return () => {
             clearInterval(x);
